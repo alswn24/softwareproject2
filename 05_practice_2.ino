f5531508b1902f5c38d08b8861be3b91209e5cb1
Jeong minju <155308068+alswn24@users.noreply.github.com>
2024-09-22 22:15:19 +0900

    Create 05_practice_2.ino
    labs : 05_practice_2

---
+++ 05_practice_2.ino
@@ -0,0 +1,25 @@
+#define PIN_LED 7
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+  // LED를 키고 1초 대기:
+  digitalWrite(PIN_LED, 1);
+  delay(1000);
+  
+  // 1초 동안 LED가 5번 깜빡임:
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+  }
+
+  // LED를 꺼진 상태로 유지 시킴:
+  digitalWrite(PIN_LED, 0);
+  while (true) {
+  //무한 루프
+  }
+}
