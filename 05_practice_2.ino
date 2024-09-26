467ea488d1f85169c6597830f92d84b38eafe0dc
Jeong minju <155308068+alswn24@users.noreply.github.com>
2024-09-26 12:10:58 +0900

    Update 05_practice_2.ino
    active low

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -6,19 +6,19 @@ void setup() {
 
 void loop() {
   // LED를 키고 1초 대기:
-  digitalWrite(PIN_LED, 1);
+  digitalWrite(PIN_LED, 0);
   delay(1000);
   
   // 1초 동안 LED가 5번 깜빡임:
   for (int i = 0; i < 5; i++) {
-    digitalWrite(PIN_LED, 0);
-    delay(100);
     digitalWrite(PIN_LED, 1);
+    delay(100);
+    digitalWrite(PIN_LED, 0);
     delay(100);
   }
 
   // LED를 꺼진 상태로 유지 시킴:
-  digitalWrite(PIN_LED, 0);
+  digitalWrite(PIN_LED, 1);
   while (true) {
   //무한 루프
   }
